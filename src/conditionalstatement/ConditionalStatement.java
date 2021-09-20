f07745e7c419781dc211bd802fef38d45b7c90f9
AbdulMoizChishti <moaiz599@gmail.com>
2021-09-21 04:07:20 +0500

    Conditional Statements

---
+++ src/conditionalstatement/ConditionalStatement.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conditionalstatement;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class ConditionalStatement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        
+//        String un ="admin";
+//        String ps="abcxyz123";
+//        
+//        if (un == "admin" && ps == "abcxyz123"){
+//            System.out.println("username and password is correct");
+//        }
+//        else{
+//            System.out.println("un or ps is incorrect");
+//        }
+
+{
+double exp=5;
+if (exp ==0 || exp<=2){
+    System.out.println("Salary is 35000");
+}
+else if (exp >2 && exp<4){
+    System.out.println("Salary is 70000");        
+       }
+else if (exp>4){
+    System.out.println("Salary is 100000");   
+}
+    }
+int num=5;
+
+if (num%2==0);{
+        System.out.println("the number is even ");
+    
+        System.out.println("The number is odd");}
+    
+//int num=6;
+//    if(num%2==0){
+//    System.out.println("the number is even ");}
+//    else{
+//    System.out.println("the number is odd ");}
+    
+    
+    }
+}
